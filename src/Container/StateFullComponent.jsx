e8fc8b162154326aa1949fd644c67115b09dfbff
HilmanShinichi <hilmanseptiana19@gmail.com>
2023-03-30 13:46:50 +0700

    statless dan statefullcomponent

---
+++ src/Container/StateFullComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+class StateFullComponent extends React.Component {
+    render() {
+      return <h1>StatefullComponent</h1>;
+    }
+  }
+
+  export default StateFullComponent
